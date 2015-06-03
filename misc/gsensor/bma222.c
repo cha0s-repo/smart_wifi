2646cbe568a878211aaabd021eeef0f3f3508055
kyle wong <arrogone@gmail.com>
2015-06-03 11:18:26 +0800

    *)add bma222 gsensor driver

---
+++ misc/gsensor/bma222.c
@@ -0,0 +1,142 @@
+//*****************************************************************************
+// BMA222drv.c - Accelerometer sensor driver APIs.
+//
+// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/ 
+// 
+// 
+//  Redistribution and use in source and binary forms, with or without 
+//  modification, are permitted provided that the following conditions 
+//  are met:
+//
+//    Redistributions of source code must retain the above copyright 
+//    notice, this list of conditions and the following disclaimer.
+//
+//    Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the 
+//    documentation and/or other materials provided with the   
+//    distribution.
+//
+//    Neither the name of Texas Instruments Incorporated nor the names of
+//    its contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+//! \addtogroup oob
+//! @{
+//
+//*****************************************************************************
+#include <stdio.h>
+#include <math.h>
+#include "bma222drv.h"
+// Common interface includes
+#include "i2c_if.h"
+#include "uart_if.h"
+
+
+//*****************************************************************************
+//                      MACRO DEFINITIONS
+//*****************************************************************************
+#define FAILURE                 -1
+#define SUCCESS                 0
+#define RET_IF_ERR(Func)        {int iRetVal = (Func); \
+                                 if (SUCCESS != iRetVal) \
+                                     return  iRetVal;}
+#define DBG_PRINT               Report
+
+//****************************************************************************
+//                      LOCAL FUNCTION DEFINITIONS                          
+//****************************************************************************
+
+
+//****************************************************************************
+//
+//! Returns the value in the specified register
+//!
+//! \param ucRegAddr is the offset register address
+//! \param pucRegValue is the pointer to the register value store
+//! 
+//! This function  
+//!    1. Returns the value in the specified register
+//!
+//! \return 0: Success, < 0: Failure.
+//
+//****************************************************************************
+int
+GetRegisterValue(unsigned char ucRegAddr, unsigned char *pucRegValue)
+{
+    //
+    // Invoke the readfrom  API to get the required byte
+    //
+    if(I2C_IF_ReadFrom(BMA222_DEV_ADDR, &ucRegAddr, 1,
+                   pucRegValue, 1) != 0)
+    {
+        DBG_PRINT("I2C readfrom failed\n\r");
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+//****************************************************************************
+//
+//! Sets the value in the specified register
+//!
+//! \param ucRegAddr is the offset register address
+//! \param ucRegValue is the register value to be set
+//! 
+//! This function  
+//!    1. Returns the value in the specified register
+//!
+//! \return 0: Success, < 0: Failure.
+//
+//****************************************************************************
+int
+SetRegisterValue(unsigned char ucRegAddr, unsigned char ucRegValue)
+{
+    unsigned char ucData[2];
+    //
+    // Select the register to be written followed by the value.
+    //
+    ucData[0] = ucRegAddr;
+    ucData[1] = ucRegValue;
+    //
+    // Initiate the I2C write
+    //
+    if(I2C_IF_Write(BMA222_DEV_ADDR,ucData,2,1) == 0)
+    {
+        return SUCCESS;
+    }
+    else
+    {
+        DBG_PRINT("I2C write failed\n\r");
+    }
+
+    return FAILURE;
+}
+
+int
+BMA222Open()
+{
+    /* set intr function, mapping to int1 to GPIO_13.
+       when (any)motions detected, GPIO_13 will be pulled up */
+    SetRegisterValue(0x16, 0xfe);
+    SetRegisterValue(0x17, 0x1f);
+    SetRegisterValue(0x19, 0xff);
+
+    return SUCCESS;
+}
